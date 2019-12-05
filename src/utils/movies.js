88046a4d8854583731aeede67d25fbb25c84fdd5
Marcus <marcus.kalmar@gmail.com>
2019-12-05 16:27:19 +0100

    Add genre to movie objects

--- src/utils/movies.js
+++ src/utils/movies.js
@@ -1,52 +1,62 @@
 export const movies = [
     {
         name: 'Kan vi göra det själva?', 
+        genre: 'Documentary',
         uri: 'https://player.vimeo.com/video/97210026', 
         description: "Trailer för Kan vi göra det själva? (2015) som har visats på svensk TV, i Riksdagen och inkluderats i Film For Action's lista 'The Top 100 Documentaries We Can Use To Change The World'."
     }
     ,
     {
-        name: 'Omsorg - om framtiden', 
+        name: 'Omsorg - om framtiden',
+        genre: 'Documentary', 
         uri: 'https://player.vimeo.com/video/195768927', 
         description: 'Kortfilm om framtidens äldreomsorg. Producerat på uppdrag av Institutet för framtidsstudier.'
     },
     {
-        name: 'Agneta', 
+        name: 'Agneta',
+        genre: 'Documentary',
         uri: 'https://player.vimeo.com/video/208637653', 
         description: 'Trailer för dokumentärfilmen Agneta (2018) som hade premiär på Tromsø international film festival.'
     },
     {
         name: 'Projekt Framsteg - Champlain Housing Trust', 
+        genre: 'Documentary',
         uri: 'https://player.vimeo.com/video/255924729', 
         description: 'En reportagefilm från Burlington, Vermont om stadens unika system för markägande. Producerad för Projekt framsteg.'
     },
     {
         name: 'Personalförvärv av företag', 
+        genre: 'Documentary',
         uri: 'https://player.vimeo.com/video/260076744', 
         description: 'En pedagogisk animerad film om hur personalförvärv kan användas som ett alternativ vid generationsskiften i företaget.'
     },
     {
         name: 'Magelungen', 
+        genre: 'Documentary',
         uri: 'https://player.vimeo.com/video/274854867', 
         description: 'En kort reportagefilm om det personalägda skolföretaget Magelungen.'
     },
     {
         name: 'Svung - hur ser din drömvärld ut?', 
+        genre: 'Documentary',
         uri: 'https://player.vimeo.com/video/314024590', 
         description: 'En film om för sociala medier om ungdomars stora visioner och drömmar. Producerad för Sveriges Ungdomsråd.'
     },
     {
         name: 'Svung - organisera dig!', 
+        genre: 'Documentary',
         uri: 'https://player.vimeo.com/video/311184487', 
         description: 'En film för sociala medier om varför ungdomar ska organisera sig. Producerad för Sveriges Ungdomsråd.'
     },
     {
         name: 'Heaven Group', 
+        genre: 'Documentary',
         uri: 'https://player.vimeo.com/video/107875433', 
         description: 'Heaven Group är en kortfilm om en konkurrensutsatt eftervärld. Premiär på Comedy Cluj International Film Festival.'
     },
     {
         name: 'This is mine',
+        genre: 'Documentary',
         uri: 'https://player.vimeo.com/video/199698515',
         description: 'Trailer för en ännu ej producerad film om bostadskrisen i Detroit.'
     }
