bea7edbbadd12f0978b2eb53ff613263493c342f
Marcus <marcus.kalmar@gmail.com>
2019-12-05 10:24:23 +0100

    Add new favicon and new assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+
 import './App.css';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
       </header>
     </div>
