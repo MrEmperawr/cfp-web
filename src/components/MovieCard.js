f5438663c8c8d152dfcb685b5b74ca5301e3b7e9
Marcus <marcus.kalmar@gmail.com>
2019-12-06 13:39:15 +0100

    Add dark mode styling to cards

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 function MovieCard({name, description, genre, image, selectMovieToPlay}) {
     return (
-        <div className="card" style={{width: '20rem', margin: '10px 10px 10px 10px'}}>
+        <div className="card bg-dark" style={{width: '20rem', margin: '10px 10px 10px 10px'}}>
             <img src={image} alt="" className="card-img-top"/>
             <div className="card-body">
-                <h4 className="card-title"><b>{name}</b></h4>
-                <p className="card-text">{description}</p>
+                <h4 className="card-title text-light"><b>{name}</b></h4>
+                <p className="card-text text-light">{description}</p>
             </div>
-            <p style={{bottom: 0}}>{genre}</p>
-            <button className="btn btn-primary" id={name} onClick={selectMovieToPlay}>
+            <p className="text-secondary">{genre}</p>
+            <button className="btn btn-secondary" id={name} onClick={selectMovieToPlay}>
                 <i 
                     style={{fontSize: 36, flexDirection: 'flex-'}} 
                     className="fa"
